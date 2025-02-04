b33c14a9806a52ac69a64b121038b8dd500e63ae
oosij <94098546+oosij@users.noreply.github.com>
2025-02-04 15:02:17 +0900

    Update clean_func.py

--- src/clean_func.py
+++ src/clean_func.py
@@ -3,13 +3,13 @@ import re
 def emoji_clean(text):
     emoji_pattern = re.compile(
     "["
-    u"\U0001F600-\U0001F64F"  # emoticons
-    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-    u"\U0001F680-\U0001F6FF"  # transport & map symbols
-    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    u"\U00002500-\U00002BEF"  # chinese char
-    u"\U0001F900-\U0001F9FF"  # supplemental symbols and pictographs
-    u"\U0001FA70-\U0001FAFF"  # additional symbols
+    u"\U0001F600-\U0001F64F"  
+    u"\U0001F300-\U0001F5FF"  
+    u"\U0001F680-\U0001F6FF" 
+    u"\U0001F1E0-\U0001F1FF" 
+    u"\U00002500-\U00002BEF" 
+    u"\U0001F900-\U0001F9FF"  
+    u"\U0001FA70-\U0001FAFF" 
     "]+", flags=re.UNICODE)
 
     cleaned_chat_log = emoji_pattern.sub(r'', text)
